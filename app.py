6c4de988e6cf4fcc02dabb6bf57f332d54a0d478
김재성 <zhqmfkv@naver.com>
2022-01-11 18:40:08 +0900

    템플릿 추가 수정

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
+# 블루프린트 import 꼭 하기
 from views import views_template, test
 
 client = MongoClient("mongodb+srv://<id>:<password>>@cluster0.kgq1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -12,16 +13,13 @@ db = client.test
 
 
 @app.route('/')
-def main():  # put application's code here
-
+def main():
     return render_template('./test_page/page_0.html')
 
 
+# 블루프린트 등록하는 부분 app.register_blueprint(블루프린트이름.bp)
 app.register_blueprint(test.bp)
 app.register_blueprint(views_template.bp)
-# app.register_blueprint(auth.bp)
-# app.register_blueprint(post.bp)
-# app.register_blueprint(mypage.bp)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
