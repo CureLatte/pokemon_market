b8ac0aa9b6f96419b8d9ad5ef8f3759ab880d5d8
김재성 <zhqmfkv@naver.com>
2022-01-12 22:16:30 +0900

    DB 설정 맞추기 작업

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
+
+
+client = MongoClient("mongodb+srv://test:sparta@cluster0.cpg4z.mongodb.net/Cluster0?retryWrites=true&w=majority")
+
 # 블루프린트 import 꼭 하기
 from views import views_template, test
-
-client = MongoClient("mongodb+srv://<id>:<password>>@cluster0.kgq1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 app = Flask(__name__)
 app.secret_key = 'sparta'
 
-db = client.test
+db = client.dbpokemon
 
 
 @app.route('/')
