580556cbc27441facae494366eacdcfd016a2bc5
joneheart <tladlf33@naver.com>
2022-01-13 14:56:58 +0900

    생성  - upload_pokemon 자바스크립트 최초 생성

--- static/javascript/upload_pokemon.js
+++ static/javascript/upload_pokemon.js
@@ -1,30 +1,35 @@
 function posting() {
     let photo = $('#input_writing_image')[0].files[0]
     let desc = $('#desc').val()
+    let title = $('#sub-title').val()
+    let price = $('#price').val()
     let form_data = new FormData()
 
 
     if(typeof photo === "undefined")
         return alert("이미지을 넣어 주세요")
+    if(desc == "")
+        return alert("내용을 입력하세요")
+    if(title == "")
+        return alert("제목을 입력하세요")
 
 
     form_data.append("photo_give", photo)
     form_data.append("desc_give", desc)
-
-
-
+    form_data.append("title_give", title)
+    form_data.append("price_give", price)
 
 
     $.ajax({
         type: "POST",
-        url: "/writing_new",
+        url: "/upload_pokemon",
         data: form_data,
         cache: false,
         contentType: false,
         processData: false,
         success: function (response) {
             alert(response["msg"])
-            window.location.href='/index_page'
+            window.location.reload()
         }
     });
 }
@@ -44,3 +49,24 @@ const inputImage = document.getElementById("input_writing_image")
 inputImage.addEventListener("change", e => {
     readImage(e.target)
 })
+function confirming(){
+    let photo = $('#input_writing_image')[0].files[0]
+    let form_data = new FormData()
+
+
+    form_data.append("photo_give", photo)
+
+    $.ajax({
+        type: "POST",
+        url: "/upload_pokemon/confirming",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert(response["msg"])
+            window.location.href='/index_page'
+        }
+    });
+}
+
