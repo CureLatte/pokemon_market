347fa8e41f0b36fcbd3e42c99878db26a6dd7455
joneheart <tladlf33@naver.com>
2022-01-13 14:58:28 +0900

    수정 - 블루프린트 사용을 위해 수정

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 
-
+from views import upload_pokemon
 client = MongoClient("mongodb+srv://test:sparta@cluster0.cpg4z.mongodb.net/Cluster0?retryWrites=true&w=majority")
 
 # 블루프린트 import 꼭 하기
@@ -19,6 +19,7 @@ def main():
 
 
 # 블루프린트 등록하는 부분 app.register_blueprint(블루프린트이름.bp)
+app.register_blueprint(upload_pokemon.bp)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
