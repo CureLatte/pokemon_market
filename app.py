0e8b3e164f87819b1fb3816a2ce91d8acffd1b74
김재성 <zhqmfkv@naver.com>
2022-01-14 00:44:56 +0900

    추가 - common 토큰 확인 API 추가

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request,redirect,jsonify,url_for
 from pymongo import MongoClient
 # 작성해야하는 부분
 from views import model_test
+import jwt
 
 
 
@@ -22,6 +23,22 @@ app.register_blueprint(model_test.bp)
 @app.route('/')
 def main():
     return render_template('./index.html')
+
+
+@app.route('/token_check', methods=['GET'])
+def token_check():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        if token_receive is not None:
+            payload = jwt.decode(token_receive, app.secret_key, algorithms=['HS256'])
+            return jsonify({'user_id': payload['user_id']})
+        else:
+            return jsonify({'user_id': 'None'})
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("sign_in", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return render_template('sign_in.html')
 
 
 if __name__ == '__main__':
