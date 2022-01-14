c936cdf14234d08c7813c606f78e93c8ba5da013
Ho Kim <hopao159@gmail.com>
2022-01-14 12:09:20 +0900

    로그인페이지 에서 토큰체크 api 보내고, 확인시 메인페이지로 이동 - 추가

--- static/javascript/sign_in.js
+++ static/javascript/sign_in.js
@@ -1,3 +1,18 @@
+$(document).ready(
+    function (){
+    $.ajax({
+        type: "GET",
+        url: "/common/token_check",
+        data:{},
+        success: function (response) {
+            let id = response['user_id']
+            if (id !== 'None') {
+                window.location.href = '/main_page'
+            }
+        }
+    })
+})
+
 function sign_in() {
     $.ajax({
         type: "POST",
@@ -5,25 +20,11 @@ function sign_in() {
         data: {id_give: $('#user_id').val(), pw_give: $('#user_pw').val()},
         success: function (response) {
             if (response['result'] === 'success') {
-                alert('로그인 완료!')
                 $.cookie('mytoken', response['token']);
-                window.location.href = '/'
+                window.location.href = '/main_page'
             } else {
                 $("#help-login").text('아이디 또는 비밀번호가 잘못 입력 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.')
-                alert('실패')
             }
-        }
-    })
-}
-
-function check_user(){
-    $.ajax({
-        type: "GET",
-        url: "/token_check",
-        data:{},
-        success: function (response) {
-            let id = response['user_id']
-            alert(id)
         }
     })
 }
