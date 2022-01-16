2152c8efafd2f14d7638bfbd932caaa78fc43d1f
joneheart <tladlf33@naver.com>
2022-01-16 13:54:16 +0900

    수정 - 기본틀 수정 및 db 데이터 가져오기 완료

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ from pymongo import MongoClient
 # 작성해야하는 부분
 # from views import model_test
 from views import common, sign_in, profile
+from views import model_test
 import jwt
+import certifi
+from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://test:sparta@cluster0.cpg4z.mongodb.net/Cluster0?retryWrites=true&w=majority")
+
+ca = certifi.where()
+
+client = MongoClient("mongodb+srv://test:sparta@cluster0.cpg4z.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile=ca)
 
 app = Flask(__name__)
 app.secret_key = 'sparta'
 
 db = client.dbpokemon
 
+
 # 블루프린트 등록하는 부분 app.register_blueprint(파일이름.bp)
-# app.register_blueprint(model_test.bp)
+app.register_blueprint(model_test.bp)
 app.register_blueprint(common.bp)
 app.register_blueprint(sign_in.bp)
 app.register_blueprint(profile.bp)
--- views/profile.py
+++ views/profile.py
@@ -3,7 +3,14 @@ from flask import request
 from flask import jsonify
 from flask import redirect, url_for, render_template
 from flask import Flask
+import certifi
+from pymongo import MongoClient
 
+ca = certifi.where()
+
+client = MongoClient(
+    "mongodb+srv://test:sparta@cluster0.cpg4z.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.dbpokemon
 bp = Blueprint("profile", __name__, url_prefix='/profile')
 app = Flask(__name__)
 app.secret_key = 'sparta'
@@ -11,6 +18,22 @@ app.secret_key = 'sparta'
 
 @bp.route('/home', methods=['GET'])
 def open_profile():
-    test = {'name': 'bobboy', 'point': 3000, 'my_box':[1,2,3,4]}
+    user = db.users.find_one({'user_id': 'qwer2'})
+    pokemon = list(db.market.find({'user_id': 'qwer1'}, {'_id': False}))
+    title = []
+    photo = []
+    date = []
+    monster = []
 
-    return render_template('jinja_guide.html', container=test)
+    for i in range(len(pokemon)):
+        title.append(pokemon[i]['header'])
+        photo.append(pokemon[i]['photo'])
+        date.append(pokemon[i]['date'])
+
+    for j in user['poket_box']:
+        for i in j:
+            monster.append(j[i])
+
+
+    # print(user['poket_box'][0])
+    return render_template('profile.html', container=user, mon_title=title, mon_photo=photo, mon_date=date, get_mon=monster)
