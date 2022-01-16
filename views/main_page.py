23e5c4465eb174ff358f7d1d6433fa06a1b15c14
seongolee <seongo0521@gmail.com>
2022-01-16 19:23:33 +0900

    추가 - 게시글 위치조정, 게시글 알고리즘 추가

--- views/main_page.py
+++ views/main_page.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask import Flask, render_template, jsonify, request, Blueprint
 from pymongo import MongoClient
 import certifi
+from views.common import check_decode
 
 ca = certifi.where()
 
@@ -17,6 +18,10 @@ db = client.dbpokemon
 
 @bp.route('/')
 def main_page():
+    login_user_id = check_decode()
+    temp_poket = db.users.find_one({'user_id': login_user_id}, {'_id': False, 'interest_poket': 1})
+    print(temp_poket)
+
     market_list = list(db.market.find({}, {'_id': False}))
 
     market_list.sort(key=lambda x: (x['date']), reverse=True)
@@ -26,11 +31,25 @@ def main_page():
     for market in market_list:
         temp_date = datetime.strptime(market['date'], '%Y-%m-%d-%H-%M-%S')
         temp_time = (now - temp_date)
-        photo_time = lambda x: str(x.days) + '일 전' if (x.days > 0) else str(time.gmtime(x.seconds).tm_hour) + '시간 전'
-        market['date'] = photo_time(temp_time)
+        market['date'] = get_time(temp_time)
         print(market['date'])
 
-    return render_template('main_page.html', market_list=market_list)
+    return render_template('main_page.html', market_list=market_list, login_interest_poket=temp_poket['interest_poket'])
+
+
+def get_time(tm):
+    replace_hour = time.gmtime(tm.seconds).tm_hour
+    replace_min = time.gmtime(tm.seconds).tm_min
+    if tm.days > 0:
+        replace_time = str(tm.days) + '일 전'
+    elif replace_hour > 0:
+        replace_time = str(replace_hour) + '시간 전'
+    elif replace_min > 0:
+        replace_time = str(replace_min) + '분 전'
+    else:
+        replace_time = '방금 전'
+
+    return replace_time
 
 
 @bp.route('/pokemon_search', methods=['GET'])
